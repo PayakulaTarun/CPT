2a8f272373b277ea098e7717857af5c679b09fb7
PayakulaTarun <payakulatarunchandra@gmail.com>
2025-04-28 10:54:11 +0530

    updated

--- day_2.py
+++ day_2.py
@@ -11,11 +11,19 @@ for i in range(a):
     l.append(b)
 print("The minimum value is: ",min(l))
 """
-def fact(n):
+"""def fact(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * fact(n - 1)
 number = int(input("Enter a number: "))
 result = fact(number)
-print(f"The factorial of {number} is: {result}")
+print(f"The factorial of {number} is: {result}")"""
+
+def is_palindrome(s):
+    return s == s[::-1]
+number = input("Enter a number: ")
+if is_palindrome(number):
+    print(f"{number} is a palindrome.")
+else:
+    print(f"{number} is not a palindrome.")
